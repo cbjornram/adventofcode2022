6d865a512fcc470493874b95938d3659baff8715
Christoffer Björnram <christofferbjornram@hotmail.com>
2022-12-04 19:55:31 +0100

    Add solution for Day 4 part 2

---
+++ day04/day04p2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+input="input.txt"
+n=1
+numberOfPairs=0
+
+while read -r line; do
+
+  # Get section borders for each pair
+  IFS=',' read -a elf <<< "$line"
+  IFS='-' read -a elf0Section <<< "${elf[0]}"
+  IFS='-' read -a elf1Section <<< "${elf[1]}"
+
+  # Find section overlaps
+  if ([ ${elf0Section[1]} -ge ${elf1Section[0]} ] && [ ${elf0Section[0]} -le ${elf1Section[1]} ])
+  then
+    numberOfPairs=$(($numberOfPairs+1))
+  fi
+
+  n=$(($n+1))
+done < "$input"
+
+echo "Number of poorly planned pairs: $numberOfPairs"
